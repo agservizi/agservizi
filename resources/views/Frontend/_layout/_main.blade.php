5f6dc841197d3fc307d389adafb5c77381692bce
Andrea <andicot@gmail.com>
2023-08-17 18:52:07 +0200

    wip

--- resources/views/Frontend/_layout/_main.blade.php
+++ resources/views/Frontend/_layout/_main.blade.php
@@ -193,22 +193,23 @@
                         <!--begin::Copyright-->
                         <div class="text-dark order-2 order-md-1">
                             <span class="text-gray-400 fw-semibold me-1">2023&copy;</span>
-                            <a href="https://keenthemes.com" target="_blank" class="text-gray-400 text-hover-primary">Keenthemes</a>
+                            <span class="text-gray-400 ">{{config('configurazione.tag_title')}}</span>
                         </div>
                         <!--end::Copyright-->
+                        @if(false)
+                            <!--begin::Menu-->
+                            <ul class="menu menu-gray-400 menu-hover-primary fw-semibold order-1">
+                                <li class="menu-item"><a href="https://keenthemes.com" target="_blank"
+                                                         class="menu-link px-2">About</a></li>
 
-                        <!--begin::Menu-->
-                        <ul class="menu menu-gray-400 menu-hover-primary fw-semibold order-1">
-                            <li class="menu-item"><a href="https://keenthemes.com" target="_blank"
-                                                     class="menu-link px-2">About</a></li>
+                                <li class="menu-item"><a href="https://devs.keenthemes.com" target="_blank"
+                                                         class="menu-link px-2">Support</a></li>
 
-                            <li class="menu-item"><a href="https://devs.keenthemes.com" target="_blank"
-                                                     class="menu-link px-2">Support</a></li>
-
-                            <li class="menu-item"><a href="https://1.envato.market/EA4JP" target="_blank"
-                                                     class="menu-link px-2">Purchase</a></li>
-                        </ul>
-                        <!--end::Menu-->
+                                <li class="menu-item"><a href="https://1.envato.market/EA4JP" target="_blank"
+                                                         class="menu-link px-2">Purchase</a></li>
+                            </ul>
+                            <!--end::Menu-->
+                        @endif
                     </div>
                     <!--end::Footer-->
                 </div>
